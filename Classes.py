8a2892eec4dc209c7519e8aeca9c09152511e81d
gaspardfs <136405803+gaspardfs@users.noreply.github.com>
2023-06-13 20:10:17 -0300

    Initial commit

---
+++ Classes.py
@@ -0,0 +1,58 @@
+# Classes utiles pour le jeu
+
+
+import pygame
+from pygame.locals import *
+import math
+
+class Screen():
+    def __init__(self, width, height):
+        self.camera = Camera()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((width, height))
+
+class Camera():
+    def __init__(self, position = [0, 0], scale = 1):
+        self.position = position
+        self.scale = scale
+    
+    def AddZoom(self, zoom):
+        self.scale = abs(self.scale + zoom)
+
+class Sprite():
+
+    def __init__(self, image, position = [0, 0]) -> None:
+        self.image = pygame.image.load(image)
+        #position is in the center of the image
+        self.position = position
+
+    def setScale(self, scale):
+        self.image = pygame.transform.scale(self.image, scale * (self.image.get_width() * 1.0, self.image.get_height()* 1.0))
+    
+    def draw(self, mainScreen):
+        #Centre la position du sprite relatif à ses dimensions
+        w, h = self.image.get_width(), self.image.get_height()
+        position = [self.position[0] - w / 2, self.position[1] - h / 2]
+
+        #Dessine le sprite relatif a la position de la camera
+        position = [position[0] - mainScreen.camera.position[0], position[1] - mainScreen.camera.position[1]]
+        image = self.image
+        #Dessine le sprite relatif a l'echelle de la camera
+        '''
+        scale = (mainScreen.camera.scale**2 * self.image.get_width(), mainScreen.camera.scale**2 * self.image.get_height())
+        image = pygame.transform.scale(self.image, scale)
+        
+        signx, signy = 1, 1
+        if position[0] < 0:
+            signx = -1
+        if position[1] < 0:
+            signy = -1
+
+        positionx = position[0] + ((position[0] - mainScreen.width / 2))**2 / mainScreen.width * signx
+        positiony = position[1] + ((position[1] - mainScreen.height / 2))**2 / mainScreen.height * signy
+        position = [positionx, positiony]
+        print(position)
+        #position = [math.log(abs(mainScreen.width / 2 - position[0])) * signx + position[0], math.log(abs(mainScreen.height / 2 - position[1])) * signy  + position[1]]
+        '''
+        mainScreen.screen.blit(image, tuple(position))
---
+++ main.py
@@ -0,0 +1,80 @@
+import pygame
+import os
+import sys
+import math
+from pygame.locals import *
+from Classes import *
+from Functions import *
+
+# VARIABLES
+CameraMoveSpeed = 10
+CameraScrollSpeed = 0.15
+
+mainScreen = Screen(600, 400)
+
+pygame.init()
+clock = pygame.time.Clock()
+background = pygame.Surface((1920, 1080))
+background.fill("gray")
+
+Renderer = []
+
+
+pygame.mouse.set_visible(1)
+pygame.display.set_caption("Simulation Gravitationelle (Q/Esc pour sortir)")
+
+sprite1 = Sprite("Sprites/PlanetRed.png", [20, 20])
+
+sprite2 = Sprite("Sprites/PlanetRed.png", [0, 400])
+
+sprite3 = Sprite("Sprites/PlanetRed.png", [400, 400])
+
+sprite4 = Sprite("Sprites/PlanetRed.png", [400, 0])
+
+
+Renderer.append(sprite1)
+Renderer.append(sprite2)
+Renderer.append(sprite3)
+Renderer.append(sprite4)
+
+def EventHandler():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+                pygame.quit()
+        if event.type == pygame.MOUSEWHEEL:
+            mainScreen.camera.AddZoom(CameraScrollSpeed * math.sqrt(mainScreen.camera.scale) * event.y)
+            print(mainScreen.camera.scale)
+
+    #Keyboard movement
+    keys = pygame.key.get_pressed()
+    if keys[K_w]:
+        mainScreen.camera.position[1] -= CameraMoveSpeed
+    if keys[K_s]:
+        mainScreen.camera.position[1] += CameraMoveSpeed
+    if keys[K_a]:
+        mainScreen.camera.position[0] -= CameraMoveSpeed
+    if keys[K_d]:
+        mainScreen.camera.position[0] += CameraMoveSpeed
+
+
+# Main game loop
+while True:
+    clock.tick(60)
+    mainScreen.screen.blit(background, (0, 0))
+
+
+    #position = pygame.mouse.get_pos()
+    #sprite1.position = position
+
+
+
+    EventHandler()
+
+    #Renderer
+    for sprite in Renderer:
+        sprite.draw(mainScreen)
+
+    pygame.display.update()
