dc9b5754bdfb0a9f7f4b15f378f1afc4f535c5c2
gaspardfs <136405803+gaspardfs@users.noreply.github.com>
2023-06-14 19:32:38 -0300

    Addes bodies

--- Classes.py
+++ Classes.py
@@ -5,6 +5,7 @@ import pygame
 from pygame.locals import *
 import math
 
+
 class Screen():
     def __init__(self, width, height):
         self.camera = Camera()
@@ -12,33 +13,37 @@ class Screen():
         self.height = height
         self.screen = pygame.display.set_mode((width, height))
 
+
 class Camera():
-    def __init__(self, position = [0, 0], scale = 1):
+    def __init__(self, position=[0, 0], scale=1):
         self.position = position
         self.scale = scale
-    
+
     def AddZoom(self, zoom):
         self.scale = abs(self.scale + zoom)
 
+
 class Sprite():
 
-    def __init__(self, image, position = [0, 0]) -> None:
+    def __init__(self, image, position=[0, 0]) -> None:
         self.image = pygame.image.load(image)
-        #position is in the center of the image
+        # position is in the center of the image
         self.position = position
 
     def setScale(self, scale):
-        self.image = pygame.transform.scale(self.image, scale * (self.image.get_width() * 1.0, self.image.get_height()* 1.0))
-    
+        self.image = pygame.transform.scale(
+            self.image, scale * (self.image.get_width() * 1.0, self.image.get_height() * 1.0))
+
     def draw(self, mainScreen):
-        #Centre la position du sprite relatif à ses dimensions
+        # Centre la position du sprite relatif à ses dimensions
         w, h = self.image.get_width(), self.image.get_height()
         position = [self.position[0] - w / 2, self.position[1] - h / 2]
 
-        #Dessine le sprite relatif a la position de la camera
-        position = [position[0] - mainScreen.camera.position[0], position[1] - mainScreen.camera.position[1]]
+        # Dessine le sprite relatif a la position de la camera
+        position = [position[0] - mainScreen.camera.position[0],
+                    position[1] - mainScreen.camera.position[1]]
         image = self.image
-        #Dessine le sprite relatif a l'echelle de la camera
+        # Dessine le sprite relatif a l'echelle de la camera
         '''
         scale = (mainScreen.camera.scale**2 * self.image.get_width(), mainScreen.camera.scale**2 * self.image.get_height())
         image = pygame.transform.scale(self.image, scale)
@@ -56,3 +61,15 @@ class Sprite():
         #position = [math.log(abs(mainScreen.width / 2 - position[0])) * signx + position[0], math.log(abs(mainScreen.height / 2 - position[1])) * signy  + position[1]]
         '''
         mainScreen.screen.blit(image, tuple(position))
+
+
+class Body():
+    def __init__(self, position=[0, 0], momentum=[0, 0], mass=0, sprite=None):
+        self.position = position
+        self.momentum = momentum
+        self.mass = mass
+        self.sprite = sprite
+
+    def draw(self, mainScreen):
+        if self.sprite != None:
+            self.sprite.draw(mainScreen)
