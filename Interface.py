6343b46aaeb0fc241d8a2af6c7cf3e7c01a3558c
AKTY74 <121370170+AKTY74@users.noreply.github.com>
2023-07-22 22:08:43 -0300

    Presets functions

--- Interface.py
+++ Interface.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from tkinter import filedialog
+import os
+
 
 def Interface(variable1):
 
@@ -31,10 +34,26 @@ def Interface(variable1):
         label_guide.place(x = 200, y = 200)
 
     def appuyer_edit(event):
+        def appuyer_presets(event):
+            """Opens NBodyPi\Presets"""
+            filename = filedialog.askopenfilename(initialdir="Presets")
+            with open(filename) as charged_preset:
+                lines = charged_preset.readlines()
+            print(lines)
+
+        def sauver(event):
+            position = entree_position.get()
+            direction = entree_direction.get()
+            vitesse = entree_vitesse.get()
+
+
         btn_regles.place(x = 10, y = 0)
         btn_edit.place(x = 60, y = 0)
         btn_sim.place(x = 100, y = 0)
-        
+
+        btn_presets = tk.Button(fenetre, text = "Charger un preset")
+        btn_presets.place(x = 200, y = 150)
+
         label_pos = tk.Label(fenetre, text = "Position")
         label_pos.place(x = 200, y = 200)
         
@@ -52,8 +71,14 @@ def Interface(variable1):
         
         entree_vitesse = tk.Entry(fenetre, width = 50)
         entree_vitesse.place(x = 255, y = 300)
+
+        btn_save = tk.Button(fenetre, text = "Sauver")
+        btn_save.place(x = 255, y = 350)
+
+        btn_save.bind("<Button-1>", sauver)
+        btn_presets.bind("<Button-1>", appuyer_presets)
         
-        
+
     # def appuyer_sim():
 
     btn_regles.bind("<Button-1>", appuyer_regles)
