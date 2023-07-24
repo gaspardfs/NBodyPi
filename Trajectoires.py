1417637e4d3bb6dc396ab025882953e78f6f52bb
gaspardfs <gaspardfsciarretta@gmail.com>
2023-07-24 12:03:00 -0300

    Trajectoires.py oubliee

--- Trajectoires.py
+++ Trajectoires.py
@@ -10,7 +10,7 @@ def calculerPositions(bodies, stepSize, steps):
 
         # Calcule le nouveau momentum
         prochainesPositions = []
-        for body in positions[-1]:
+        for body in positions[len(positions) - 1]:
             prochainesPositions += [copy.copy(body)]
 
         prochainesPositions = LoiGravitation.apply(prochainesPositions, stepSize)
@@ -18,23 +18,28 @@ def calculerPositions(bodies, stepSize, steps):
         # Calcule les nouvelles positions
         for body in prochainesPositions:
             body.position = [body.position[0] + body.momentum[0], body.position[1] + body.momentum[1]]
-            #print(body.momentum)
+            #print(body.position)
             
         positions.append(prochainesPositions)
         #print(positions[-1][0].position)
+    tout = []
+    for body in positions:
+        pos = []
+        for posit in body:
+            pos += [posit.position]
+        tout += [pos]
 
-    return(positions)
+    return(tout)
 
 def dessinerLignes(positions, mainScreen, couleursDesCorps):
-    for bodyI in range(len(couleursDesCorps)):
-        for positionI in range(len(positions[bodyI]) - 1):
-            pos1 = positions[bodyI][positionI]
-            pos2 = positions[bodyI][positionI + 1]
-            pos1, scale = mainScreen.camera.GetTransformFromCamera(pos1.position)
-            pos2, scale = mainScreen.camera.GetTransformFromCamera(pos2.position)
-
+    for positionI in range(len(positions) - 1):
+        for bodyI in range(len(positions[positionI])):
+            pos1 = positions[positionI][bodyI]
+            pos2 = positions[positionI + 1][bodyI]
+            pos1, scale = mainScreen.camera.GetTransformFromCamera(pos1)
+            pos2, scale = mainScreen.camera.GetTransformFromCamera(pos2)
             # scale n'est pas utilise
-            pygame.draw.line(mainScreen.screen, couleursDesCorps[bodyI], pos1, pos2)
+            pygame.draw.line(mainScreen.screen, couleursDesCorps[bodyI], pos1, pos2, 1)
 
 
 
