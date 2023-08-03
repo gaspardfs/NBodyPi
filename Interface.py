8df1a09f8a09c6abe17c80f9f8f63da4d642542a
nunuca <manuelagiorgiteisseire@gmail.com>
2023-08-03 20:45:28 -0300

    adição de widgets + realocação das propriedades mostradas quando os botões edit ou regles são apertados + reconfiguração dos botões para grid

--- Interface.py
+++ Interface.py
@@ -6,50 +6,59 @@ def Interface(variable1):
 
     fenetre.geometry("400x400")
 
+    #Création des bouttons
     btn_regles = tk.Button(fenetre, text ="Règles")
-    btn_regles.place(x = 10, y = 0)
+    btn_regles.grid(column = 0, row = 0)
 
     btn_edit = tk.Button(fenetre, text = "Edit")
-    btn_edit.place(x = 60, y = 0)
+    btn_edit.grid(column = 1, row = 0)
 
     btn_sim = tk.Button(fenetre, text = "Sim")
-    btn_sim.place(x = 100, y = 0)
+    btn_sim.grid(column = 2, row = 0)
 
-    champ_vitesse = tk.Entry(fenetre, width = 50)
-    champ_vitesse.place()
+    # champ_vitesse = tk.Entry(fenetre, width = 50)
+    # champ_vitesse.grid(column = , row =)
 
+#Conséquence d'appuyer sur les buttons
     def appuyer_regles(event):
-        guide = ""
-        
+        btn_base = tk.Button(fenetre, text = "base")
+        btn_base.grid(column = 0, row = 50)
+    
+        guide = "hello"
+    
         label_guide = tk.Label(fenetre, text = "")
         label_guide.config(text = guide)
         label_guide.place(x = 200, y = 200)
 
     def appuyer_edit(event):
-        
+    
         widget_edit = tk.Frame(fenetre)
-        
+    
+        btn_base = tk.Button(widget_edit, text = "base") 
+        btn_base.grid(column = 0, row = 20)
+    
+       
+    
         label_pos = tk.Label(widget_edit, text = "Position")
-        label_pos.place(x = 0, y = 200)
-        
+        label_pos.grid(column = 0, row = 0)
+    
         label_direction = tk.Label(widget_edit, text = "Direction")
-        label_direction.place(x = 0, y = 250)
-        
+        label_direction.grid(column = 0, row = 1)
+    
         label_vitesse = tk.Label(widget_edit, text = "Vitesse")
-        label_vitesse.place(x = 0, y = 300)
-        
+        label_vitesse.grid(column = 0, row = 2)
+    
         entree_pos = tk.Entry(widget_edit, width = 50)
-        entree_pos.place(x = 55, y = 200)
-        
+        entree_pos.grid(column = 1, row = 0)
+    
         entree_direction = tk.Entry(widget_edit, width = 50)
-        entree_direction.place(x = 55, y = 250)
-        
+        entree_direction.grid(column = 1, row = 1)
+    
         entree_vitesse = tk.Entry(widget_edit, width = 50)
-        entree_vitesse.place(x = 55, y = 300)
+        entree_vitesse.grid(column = 1, row = 2)
 
-        
-        widget_edit.grid(column = 0, row = 3)
-        
+    
+        widget_edit.grid(column = 0, row = 60, columnspan = 3)
         
     def appuyer_sim(event):
         
