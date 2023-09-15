a716c162538578d91a44bd9f6f2187fc4b98be98
AKTY74 <121370170+AKTY74@users.noreply.github.com>
2023-09-15 18:15:42 -0300

    Resizeable screen
    Atributo "pygame.RESIZEABLE" adicionado na função __init__ da classe Screen

--- Classes.py
+++ Classes.py
@@ -12,7 +12,7 @@ class Screen:
         self.camera = Camera([width, height], [width, height])
         self.width = width
         self.height = height
-        self.screen = pygame.display.set_mode((width, height))
+        self.screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 
 
 class Camera:
